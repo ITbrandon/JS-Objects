1345b7a4079aeb5d732465b75c6231eccf07e096
Itbrandon <brandoniticka@gmail.com>
2024-01-19 18:09:33 -0800

    playground-3

--- main.js
+++ main.js
@@ -1,32 +1,7 @@
-const houseForSale = {
-  area: 940,
-  value: 320000,
-  streetName: "Fifth Street",
-  built: "2012",
-  owner: {
-    name: "Blake",
-    age: 29
-  },
-  offers: [290000, 295000, 315000, 312000]
+const myConsole = {
+  log: (message) => {
+    console.log(message);
+  }
 }
 
-console.log(houseForSale);
-
-delete houseForSale.built;
-
-houseForSale.owner.age = 30;
-
-const maxOffer = houseForSale.offers.reduce((accumulator, currentValue) => {
-  return Math.max(accumulator, currentValue);
-}, houseForSale.offers[0])
-
-console.log(maxOffer);
-
-houseForSale["Sales Price"] = 312000;
-
-console.log(houseForSale);
-
-
-
-
-
+myConsole.log("Hello, Friend");
