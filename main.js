7f1de4cbdd8ce56d822fdc3be5c371d8ca004998
Itbrandon <brandoniticka@gmail.com>
2024-01-20 16:58:31 -0800

    playground-4

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-const myConsole = {
-  log: (message) => {
-    console.log(message);
+class student {
+  constructor(name, major, grades) {
+    this.name = name;
+    this.major = major;
+    this.grades = grades;
+  }
+
+  addGrades(grade) {
+    this.grades.push(grade);
+  }
+
+  gpa() {
+    let sum = 0
+    for(let i = 0; i < this.grades.length; i++)
+    {
+      sum += this.grades[i];
+    }
+
+    let average = sum/this.grades.length;
+
+    return average;
   }
 }
 
-myConsole.log("Hello, Friend");
+const eva = new student("Eva", "Arts", [95,75,83]);
+
+console.log(eva);
+
+eva.addGrades(90)
+
+console.log(eva.gpa());
